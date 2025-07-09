b94fdec0656a31ee861b1502dcacd4751b4a0922
FBigT <110102755+BilloDoom@users.noreply.github.com>
2025-07-09 10:45:28 +0200

    port sync deplyoment

--- backend/src/main/java/good/stuff/backend/config/security/SecurityConfig.java
+++ backend/src/main/java/good/stuff/backend/config/security/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://algebra-javaweb-static.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
